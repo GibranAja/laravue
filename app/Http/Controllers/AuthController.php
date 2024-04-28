ac88d5c18ee1d0a76f21694d021e19f63d521d18
GibranAja <mgibranarrffi@gmail.com>
2024-04-29 05:42:21 +0700

    commit ketiga

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,7 +19,7 @@ class AuthController extends Controller
 
         if (! $user || ! Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
-                'email' => ['The provided credentials are incorrect.'],
+                'email' => ['Ada sesuatu yang salah.'],
             ]);
         };
         
