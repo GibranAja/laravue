c4ea55bc94c007b92cb98dc0369bc43873112712
GibranAja <mgibranarrffi@gmail.com>
2024-04-29 06:03:23 +0700

    commit keempat

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request) {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['Ada sesuatu yang salah.'],
+            ]);
+        };
+        
+        unset($user->email_verified_at);
+        unset($user->created_at);
+        unset($user->updated_at);
+        unset($user->deleted_at);
+
+        $user->tokens()->delete();
+        $token = $user->createToken('sanctum')->plainTextToken;
+        $user->token = $token;
+
+        return response(['data' => $user]);
+    }
+
+    public function logout() {
+        $user = auth()->user();
+        $user->tokens()->delete();
+
+        return response(['Message' => 'Logout succes']);
+    }
+
+    public function me() {
+        return response(['data' => auth()->user()]);
+    }
+}
